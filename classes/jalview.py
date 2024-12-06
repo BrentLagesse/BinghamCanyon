0490b78ca5e4eb31609fad67aee404df9fb32a98
Adam Chhor <chhoradam@gmail.com>
2024-12-05 22:03:30 -0800

    fixed jalview not opening and cleanedup open method

--- classes/jalview.py
+++ classes/jalview.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 import subprocess
 
-class Jalview():
 
+class Jalview:
     """To do: handle different OS"""
-    exe_path : str
-    is_window : bool
-    def __init__(self, exe_path :Path, is_window = True):
+
+    exe_path: str
+    is_window: bool
+
+    def __init__(self, exe_path: Path, is_window=True):
         self.exe_path = exe_path
         self.is_window = is_window
-        # run(['jalview_exe_path', "https://www.ebi.ac.uk/Tools/services/rest/clustalo/result/clustalo-R20240709-165430-0694-644924-p1m/aln-clustal_num"], shell = True)
-        
-    def open(self, ebi_url :str = ''):
-        subprocess.Popen([self.exe_path, ebi_url], shell = self.is_window)
+
+    def open(self, ebi_url: str = ""):
+        # example of ebi_url "https://www.ebi.ac.uk/Tools/services/rest/clustalo/result/clustalo-R20240709-165430-0694-644924-p1m/aln-clustal_num"
+        subprocess.Popen([self.exe_path, ebi_url], shell=self.is_window)
