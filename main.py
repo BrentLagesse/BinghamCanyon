ddfc37cff7ee0bb4f6900fd78ee9e172e8182a31
Adam Chhor <chhoradam@gmail.com>
2024-12-05 21:57:24 -0800

    changed all options to use a config file instead

--- main.py
+++ main.py
@@ -8,46 +8,49 @@ from classes import (
     AlphaFoldRESTAPI,
     Jalview,
     Chimerax,
-    SSSParseTest,
+    SSSTest,
     DBFetch,
     NCIBlastPlusParseTest,
 )
-
-# from classes.job_dispatcher import JobDispatcherRESTAPI
-# from classes.sequence_similarly_search import NCIBlASTPlus
-# from classes.multiple_sequence_alignment import ClustalOmega
+from utils import Config
 
 
 PRO_MODEL = AlphaFoldRESTAPI()
-
-chimerax = Chimerax("C:\\Program Files\\ChimeraX 1.8\\bin\chimerax.exe", True)
-
-DB_fetch = DBFetch(is_individually_retrieved=True)
-SSS = NCIBlASTPlus(
-    job_dispatcher=JobDispatcherRESTAPI("ncbiblast"),
-    sequence_database=DB_fetch,
-    check_delay=10,
-)
-# SSS = NCIBlastPlusParseTest(
-#     job_dispatcher=JobDispatcherRESTAPI("ncbiblast"),
-#     sequence_database=DB_fetch,
-#     check_delay=10,
-# )
-# SSS = SSSTest(sequence_database=DB_fetch)
-MSA = ClustalOmega(job_dispatcher=JobDispatcherRESTAPI("clustalo"), check_delay=5)
+conf = Config.load_json("config.json")
+chimerax = Chimerax(exe_path=conf.chimerax.exe_path, is_window=True)
 jalview = Jalview(
-    exe_path="C:\\Users\\chhor\\AppData\\Local\\Jalview\\bin\\jalview.bat",
+    exe_path=conf.jalview.exe_path,
     is_window=True,
 )
 
+seq_db = DBFetch(
+    is_individually_retrieved=conf.sequence_database.db_fetch.is_individually_retrieved
+)
+SSS = NCIBlASTPlus(
+    job_dispatcher=JobDispatcherRESTAPI("ncbiblast"),
+    sequence_database=seq_db,
+    check_delay=conf.sequence_similarly_search.nci_blast_plus.check_delay,
+)
+# SSS = NCIBlastPlusParseTest(
+#     job_dispatcher=JobDispatcherRESTAPI("ncbiblast"),
+#     sequence_database=seq_db,
+#     check_delay=conf.sequence_similarly_search.nci_blast_plus.check_delay,
+# )
+# SSS = SSSTest(sequence_database=seq_db)
+MSA = ClustalOmega(
+    job_dispatcher=JobDispatcherRESTAPI("clustalo"),
+    check_delay=conf.multiple_sequence_alignment.clustal_omega.check_delay,
+)
+
 INPUT_MODE = False
-output_folder_path = Path("output")
+output_folder_path = Path(conf.output_folder)
 
 
 def main():
+    print(conf.input_mode)
     # Iterate through all files in the folder and delete them
     for file in output_folder_path.iterdir():
-        if file.is_file():  # Check if it is a file
+        if file.is_file():
             file.unlink()  # Delete the file
 
     UNIPROT_ENTRY = "P54199"
@@ -62,8 +65,8 @@ def main():
     sss_result_path, sss_job_id = SSS.run(seq, settings="NOT YET IMPLEMENTED")
     sss_fasta_path = SSS.parse(
         sss_result_path=sss_result_path,
-        target_match=50,
-        max_entries=20,
+        target_match=conf.sequence_similarly_search.parse.target_match,
+        max_entries=conf.sequence_similarly_search.parse.max_entries,
     )
     # aligns DNA and returns jalview url
     jalview_url = MSA.run(sss_fasta_path, settings="NOT YET IMPLEMENTED")
